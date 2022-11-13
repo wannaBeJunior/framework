e5d545cd17d404f21e07906e49fd401ae42d60a2
Evgeniy Mukhin <taburetka2281337@gmail.com>
2022-11-13 11:05:40 +0300

    Добавил класс для Insert запросов

---
+++ app/modules/system/lib/InsertQuery.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Modules\System;
+
+use Exception;
+
+class InsertQuery extends Query
+{
+	private array $fields;
+	private array $values;
+
+	public function setFields(array $fields): self
+	{
+		$this->fields = $fields;
+		return $this;
+	}
+
+	public function setValues(array $values): self
+	{
+		$this->values = $values;
+		return $this;
+	}
+
+	protected function generateSql(): self
+	{
+		$this->sql = "INSERT INTO {TABLE} ({FIELDS}) VALUES ({VALUES});";
+		try {
+			$this->replaceTableName();
+			$this->replaceFields();
+			$this->replaceValues();
+		}catch(Exception $exception)
+		{
+			echo $exception->getMessage();
+			die();
+		}
+		return $this;
+	}
+
+	private function replaceFields()
+	{
+		if(!$this->fields)
+		{
+			throw new Exception('Add fields');
+		}
+		$fieldsPlaceholder = '{FIELDS}';
+		for($i = 0; $i < count($this->fields); $i++)
+		{
+			$fieldDelimiter = '';
+			if($i >= 0 && $i < count($this->fields) - 1)
+			{
+				$fieldDelimiter = ', ';
+			}
+			$field = $this->fields[$i];
+			$this->sql = str_replace($fieldsPlaceholder, "`{$field}`{$fieldDelimiter} {$fieldsPlaceholder}", $this->sql);
+		}
+		$this->deletePlaceholder($fieldsPlaceholder);
+	}
+
+	private function replaceValues()
+	{
+		if(!$this->fields)
+		{
+			throw new Exception('Add values');
+		}
+		$valuesPlaceholder = '{VALUES}';
+		for($i = 0; $i < count($this->values); $i++)
+		{
+			$fieldDelimiter = '';
+			if($i >= 0 && $i < count($this->values) - 1)
+			{
+				$fieldDelimiter = ', ';
+			}
+			$value = $this->values[$i];
+			$this->sql = str_replace($valuesPlaceholder, "'{$value}'{$fieldDelimiter} {$valuesPlaceholder}", $this->sql);
+		}
+		$this->deletePlaceholder($valuesPlaceholder);
+	}
+}
