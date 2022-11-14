df3fe815afa68eed3fc3de57f5813abb32a66cc9
Evgeniy Mukhin <taburetka2281337@gmail.com>
2022-11-14 17:07:53 +0300

    Добавил класс для DELETE запросов

---
+++ app/modules/system/lib/DeleteQuery.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Modules\System;
+
+use Exception;
+
+class DeleteQuery extends Query
+{
+	protected function generateSql(): self
+	{
+		$this->sql = "DELETE FROM {TABLE} {WHERE};";
+		try {
+			$this->replaceTableName();
+			$this->replaceWhere();
+		}catch (Exception $exception)
+		{
+			echo $exception->getMessage();
+			die();
+		}
+		return $this;
+	}
+}
