5cf99c81a3c94e75b841e41fdaab2e145920f8bf
Evgeniy Mukhin <taburetka2281337@gmail.com>
2022-12-23 15:19:09 +0300

    Добавил логирование в базовый Exception

--- app/modules/system/lib/exceptions/BaseException.php
+++ app/modules/system/lib/exceptions/BaseException.php
@@ -2,8 +2,23 @@
 
 namespace App\Modules\System\Exceptions;
 
+use App\Modules\System\Container\Container;
+use App\Modules\System\Logger\Logger;
 use Exception;
+use Throwable;
 
 abstract class BaseException extends Exception
 {
+	private Logger $logger;
+
+	public function __construct($message = "", $code = 0, Throwable $previous = null)
+	{
+		$this->logger = Container::getInstance()->get(Logger::class);
+		parent ::__construct($message, $code, $previous);
+	}
+
+	public function getLogger(): Logger
+	{
+		return $this->logger;
+	}
 }
