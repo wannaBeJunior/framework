ee568aee368735a66a9df0f6206bf20b27e3babc
Evgeniy Mukhin <taburetka2281337@gmail.com>
2023-05-06 21:10:45 +0300

    Добавил метод проверяющий доступ к модулю текущего пользователя.

--- app/modules/system/lib/tools/GroupsTools.php
+++ app/modules/system/lib/tools/GroupsTools.php
@@ -26,4 +26,52 @@ class GroupsTools
 		}
 		return [];
 	}
+
+	/**
+	 * Проверяет, имеют ли группы доступ к тому или иному модулю/сущности
+	 * @param string $module
+	 * @param int|array $groups
+	 * @return bool
+	 */
+	static public function checkAccessRightsByGroupIds(string $module, $groups): bool
+	{
+		if(!is_array($groups))
+		{
+			$groups = [$groups];
+		}
+
+		$accessRights = (new SelectQuery())
+			->setTableName('access_rights_groups')
+			->setJoin([
+				'type' => 'inner',
+				'ref_table' => 'access_rights',
+				'on' => 'this.access_right = ref.id'
+			])
+			->setSelect(['*']);
+		$params = [];
+		for($i = 0; $i < count($groups); $i++)
+		{
+			$accessRights->setWhere([
+				'condition' => '`group` = :group' . $i,
+				'logic' => 'OR'
+			]);
+			$params['group' . $i] = $groups[$i];
+		}
+		$accessRightsResult = $accessRights->setParams($params)
+			->execution();
+		if($accessRightsResult->isSuccess())
+		{
+			$result = [];
+			$accessRights = $accessRightsResult->getResult();
+			foreach($accessRights as $accessRight)
+			{
+				if($accessRight['module'] == $module && !isset($result[$accessRight['group']]))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		return false;
+	}
 }
