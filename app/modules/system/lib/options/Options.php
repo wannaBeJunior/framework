0da89a2c8d60ad66377f0e7e5fc8783bcddae8ce
Evgeniy Mukhin <taburetka2281337@gmail.com>
2023-05-07 11:07:51 +0300

    Добавил методы для получения настроек.

--- app/modules/system/lib/options/Options.php
+++ app/modules/system/lib/options/Options.php
@@ -18,10 +18,10 @@ class Options
 			->setTableName('options')
 			->setSelect(['*'])
 			->setWhere([
-				'condition' => 'name = :name',
+				'condition' => 'code = :code',
 			])
 			->setParams([
-				'name' => $optionName
+				'code' => $optionName
 			])
 			->execution()
 			->getResult();
@@ -58,5 +58,116 @@ class Options
 			}
 		}
 		return $option;
+	}
+
+	static public function getOptionsByModule(string $module): array
+	{
+		if(isset(static::$cache[$module]))
+		{
+			return static::$cache[$module];
+		}
+		$options = (new SelectQuery())
+			->setTableName('options')
+			->setSelect(['options.*', 'option_sections.code as "section_code"', 'option_sections.name as "section_name"'])
+			->setWhere([
+				'condition' => 'module = :module',
+			])
+			->setJoin([
+				'type' => 'inner',
+				'ref_table' => 'option_sections',
+				'on' => 'this.section = ref.id'
+			])
+			->setParams([
+				'module' => $module
+			])
+			->execution();
+		if($options->isSuccess())
+		{
+			$options = $options->getResult();
+			$enumOptions = [];
+			$textOptions = [];
+			foreach ($options as $option)
+			{
+				if($option['type'] == 'enum')
+				{
+					$enumOptions[] = $option;
+					continue;
+				}
+				$textOptions[] = $option;
+			}
+			$enumOptions = static::getEnumOptionsByOptionIds(array_column($enumOptions, 'id'));
+			$textOptions = static::getOptionsByOptionIds(array_column($textOptions, 'id'));
+			foreach ($enumOptions as $enumOption)
+			{
+				foreach ($options as &$option)
+				{
+					if($option['id'] == $enumOption['option'])
+					{
+						$option['values'][] = $enumOption;
+					}
+				}
+			}
+			unset($option);
+			foreach ($textOptions as $textOption)
+			{
+				foreach ($options as &$option)
+				{
+					if($option['id'] == $textOption['option'])
+					{
+						$option['value'] = $textOption['value'];
+					}
+				}
+			}
+			unset($option);
+			static::$cache[$module] = $options;
+			return $options;
+		}
+		return [];
+	}
+
+	static public function getEnumOptionsByOptionIds(array $ids): array
+	{
+		$options = (new SelectQuery())
+			->setSelect(['*'])
+			->setTableName('option_enums');
+		$fields = [];
+		foreach ($ids as $id)
+		{
+			$options->setWhere([
+				'condition' => '`option` = :option' . $id,
+				'logic' => 'OR'
+			]);
+			$fields['option' . $id] = $id;
+		}
+		$result = $options->setParams($fields)
+			->execution();
+		if($result->isSuccess())
+		{
+			return $result->getResult();
+		}
+		return [];
+	}
+
+	static public function getOptionsByOptionIds(array $ids): array
+	{
+		$options = (new SelectQuery())
+			->setSelect(['*'])
+			->setTableName('option_values');
+		$fields = [];
+		foreach ($ids as $id)
+		{
+			$options->setWhere([
+				'condition' => '`option` = :option' . $id,
+				'logic' => 'OR'
+			]);
+			$fields['option' . $id] = $id;
+		}
+		$result = $options->setParams($fields)
+			->execution();
+		if($result->isSuccess())
+		{
+			return $result->getResult();
+		}
+		return [];
 	}
 }
