37ca336923a9a0327325abd15d55b0d785d580e1
Evgeniy Mukhin <taburetka2281337@gmail.com>
2023-05-15 19:49:54 +0300

    Удалил console.log

--- app/admin/assets/scripts/admin.js
+++ app/admin/assets/scripts/admin.js
@@ -1,5 +1,4 @@
 $('.module').on( "mouseenter", function (e) {
-    console.log($(this));
     $(this).toggleClass('list-group-item-primary');
 } ).on( "mouseleave", function () {
     $(this).toggleClass('list-group-item-primary');
