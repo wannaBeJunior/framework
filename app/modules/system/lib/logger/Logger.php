b491361761d5d707980678cce56cd76d208a8eee
Evgeniy Mukhin <taburetka2281337@gmail.com>
2023-08-15 16:11:39 +0300

    Теперь в Logger используется класс File

--- app/modules/system/lib/logger/Logger.php
+++ app/modules/system/lib/logger/Logger.php
@@ -2,16 +2,13 @@
 
 namespace App\Modules\System\Logger;
 
+use App\Modules\System\File\File;
+
 class Logger
 {
-	protected const NOTICE_LOGFILE_PATH = '/logs/notices/';
-	protected const WARNING_LOGFILE_PATH = '/logs/warnings/';
-	protected const ERROR_LOGFILE_PATH = '/logs/errors/';
-
-	public function __construct()
-	{
-		//TODO: реализовать класс File
-	}
+	protected const NOTICE_LOGFILE_PATH = 'logs/notices/';
+	protected const WARNING_LOGFILE_PATH = 'logs/warnings/';
+	protected const ERROR_LOGFILE_PATH = 'logs/errors/';
 
 	/**
 	 * Запишет в лог файл уведомление о какой-либо лёгкой ошибке которая не влияет на работу системы.
@@ -21,7 +18,9 @@ class Logger
 	public function notice(string $message, string $file = 'sys.log'): void
 	{
 		$message = '['. date('Y-m-j G:i:s') . ' NOTICE at ' . self::getStackTraceInfo() . '] ' . $message . PHP_EOL;
-		file_put_contents($_SERVER['DOCUMENT_ROOT'] . self::NOTICE_LOGFILE_PATH . $file, $message, FILE_APPEND);
+		$file = new File(self::NOTICE_LOGFILE_PATH);
+		$file->create();
+		$file->write($message);
 	}
 
 	/**
@@ -32,7 +31,9 @@ class Logger
 	public function warning(string $message, string $file = 'sys.log'): void
 	{
 		$message = '['. date('Y-m-j G:i:s') . ' WARNING at ' . self::getStackTraceInfo() . '] ' . $message . PHP_EOL;
-		file_put_contents($_SERVER['DOCUMENT_ROOT'] . self::WARNING_LOGFILE_PATH . $file, $message, FILE_APPEND);
+		$file = new File(self::WARNING_LOGFILE_PATH);
+		$file->create();
+		$file->write($message);
 	}
 
 	/**
@@ -43,7 +44,9 @@ class Logger
 	public function error(string $message, string $file = 'sys.log'): void
 	{
 		$message = '['. date('Y-m-j G:i:s') . ' ERROR at ' . self::getStackTraceInfo() . '] ' . $message . PHP_EOL;
-		file_put_contents($_SERVER['DOCUMENT_ROOT'] . self::ERROR_LOGFILE_PATH . $file, $message, FILE_APPEND);
+		$file = new File(self::ERROR_LOGFILE_PATH);
+		$file->create();
+		$file->write($message);
 	}
 
 	static public function getStackTraceInfo(): string
